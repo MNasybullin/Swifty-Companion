b42b9ef5923c53448448af46e15b65275233c6c4
Насыбуллин Мансур Ришатович <MRNasybullin@sberbank.ru>
2021-05-20 11:20:58 +0300

    refactoring code && edited .gitignore

--- Swifty Companion/Swifty Companion/Network/Network.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Network.swift
-//  Swifty Companion
-//
-//  Created by OUT-Nasybullin-MR on 17.05.2021.
-//
-
-import UIKit
---
+++ Swifty Companion/Swifty Companion/Network/NetworkService.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkService.swift
+//  Swifty Companion
+//
+//  Created by OUT-Nasybullin-MR on 17.05.2021.
+//
+
+import UIKit
+
+protocol NetworkServiceProtocol {
+    
+}
+
+class NetworkService: NetworkServiceProtocol {
+    
+}
